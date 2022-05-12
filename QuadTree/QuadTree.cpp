cc6516d6bc5c15e0ba39ab1e4d6c57c1952fc282
wairesp <54559794+wairesp@users.noreply.github.com>
2022-05-12 00:46:25 -0500

    Update QuadTree.cpp
    minor type fix

--- QuadTree/QuadTree.cpp
+++ QuadTree/QuadTree.cpp
@@ -18,7 +18,7 @@ class QT
 
     std::vector<MyVec*> data;
 public:
-    QT(int h, int x0, int x1, int y0, int y1) {
+    QT(int h, float x0, float x1, float y0, float y1) {
         this->prof = h; // profundidad
         this->x0 = x0;
         this->y0 = y0;
